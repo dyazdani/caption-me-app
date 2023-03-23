680c7f9caab004981bcc9dfbf5affb6771644963
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-03-23 16:08:55 -0700

    Added preview of chosen image

--- scripts/main.js
+++ scripts/main.js
@@ -14,6 +14,34 @@ const imageInputElement = document.getElementById("image");
 function onImageInputChanged() {
   const selectedImageFile = imageInputElement.files[0];
   state.image = selectedImageFile.name;
+
+  renderImagePreview(selectedImageFile);
+}
+
+// ---* helper functions *---
+function renderImagePreview(selectedImageFile) {
+  if (!document.querySelector("img")) {
+    const imagePreview = document.createElement("img");
+    imagePreview.id = "image-preview";
+    imagePreview.setAttribute("src", "");
+    imagePreview.setAttribute("alt", "Image preview");
+    const main = document.querySelector("main");
+    main.appendChild(imagePreview);
+  }
+
+  const imagePreview = document.querySelector("#image-preview");
+  const reader = new FileReader();
+
+  reader.addEventListener(
+    "load",
+    () => {
+      // convert image file to base64 string
+      imagePreview.src = reader.result;
+    },
+    false
+  );
+
+  reader.readAsDataURL(selectedImageFile);
 }
 
 // ----------* EVENT LISTENERS *--------------
